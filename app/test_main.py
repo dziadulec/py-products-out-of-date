f0bdc750f3b7073eefac39cc22e26868e7ede713
Marek Drządzewski <dzadzumaster@gmail.com>
2025-03-16 15:03:58 +0100

    add tests for outdated_products function

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,39 @@
-# write your code here
+from app.main import outdated_products
+import datetime
+from unittest.mock import patch, MagicMock
+import pytest
+
+
+@pytest.mark.parametrize(
+    "product_list, today_date, expected",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2023, 2, 10),
+                    "price": 600},
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 2),
+                    "price": 120},
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                },
+            ],
+            datetime.date(2022, 2, 2),
+            ["duck"],
+        ),
+    ]
+)
+@patch("app.main.datetime.date")
+def test_outdated_products(
+        mock_date: MagicMock,
+        product_list: list,
+        today_date: datetime,
+        expected: datetime
+) -> None:
+    mock_date.today.return_value = today_date
+    assert outdated_products(product_list) == expected
